cfa8a96bee024a6a34fb648ec0ab285d9c493130
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-03-09 18:02:31 +0700

    implementasi output status kelulusan dan nilai rata-rata

--- file2.cpp
+++ file2.cpp
@@ -29,4 +29,6 @@ int main(){
     rerata = Rata_rata(nilBI,nilMT);
     //status = Status_lulus(rerata);
 
-   
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<< " ,dengan nilai rerata = "<<rerata<<endl;
+
+}
